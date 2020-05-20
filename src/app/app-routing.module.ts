9061a0d2baeda309ebef24ec0bf659ed8371f57c
angelm1974 <18185521+angelm1974@users.noreply.github.com>
2020-05-20 16:15:26 +0200

    Dodane komponenty

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './core/dashboard/dashboard.component';
+import { LoginComponent } from './core/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',redirectTo: '/login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'dashboard',component: DashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
